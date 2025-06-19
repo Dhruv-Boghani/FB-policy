06e42fb43bdfbfe10bec71760405930197837361
Dhruv Boghani <dhruvboghani624@gmail.com>
2025-06-19 12:42:06 +0530

    first commit

--- index.js
+++ index.js
@@ -3,14 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files (like index.html)
-app.use(express.static(path.join(__dirname)));
-
-// Handle GET /
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
-    console.log(`Your server is live at http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
